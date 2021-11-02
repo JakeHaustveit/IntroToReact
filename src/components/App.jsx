6e707119c522684b3c1a5907b535bdfee1ce451b
unknown <jacobhaustveit@gmail.com>
2021-11-02 15:44:46 -0400

    created buttons and functions to cycle back and forth between books.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,20 +15,50 @@ class App extends Component {
         };
     }
 
+    goToNextBook = () =>{
+        
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber ++
+        if(tempBookNumber === this.books.length){
+            tempBookNumber=0
+        }
+        this.state.bookNumber= tempBookNumber;
+
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+        
+    }
+
+    goToPreviousBook = () =>{
+        
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber --
+        if(tempBookNumber < this.books.length){
+            tempBookNumber=0
+        }
+        this.state.bookNumber= tempBookNumber;
+
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+        
+    }
+
     render(){
         return(
             <div className="container-fluid">
                 <TitleBar />
                 <div className= "row">
                 <div className= "col-md-4">
-                    <button></button>
+                    <button onClick={this.goToPreviousBook}> Previous Book </button>
                 </div>
                 <div className= "col-md-4">
                     <h1> {this.books[this.state.bookNumber].title}</h1>
                     <h1> {this.books[this.state.bookNumber].author}</h1>             
                 </div>
                 <div className= "col-md-4">
-                    <button></button>
+                    <button onClick={this.goToNextBook}> Next Book </button>
                 </div>                    
                 </div>
                 
